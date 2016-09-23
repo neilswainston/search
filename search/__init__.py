10d2228692ec2b1f922fbdeb03eb80287c22260b
Neil Swainston <neil.swainston@manchester.ac.uk>
2016-09-23 15:28:48 +0100

    Initial check-in.

---
+++ search/__init__.py
@@ -0,0 +1,127 @@
+'''
+PartsGenie (c) University of Manchester 2015
+
+PartsGenie is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
+'''
+import json
+import os
+import re
+import sys
+import tempfile
+import traceback
+import urllib
+import urllib2
+import uuid
+
+from Bio import SeqIO
+from Bio.PDB.Entity import Entity
+from flask import Flask, jsonify, request, Response
+import libchebipy
+
+from synbiochem.utils import sequence_utils, taxonomy_utils
+
+
+# Configuration:
+DEBUG = True
+SECRET_KEY = str(uuid.uuid4())
+
+# Create application:
+_STATIC_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                              'static')
+APP = Flask(__name__, static_folder=_STATIC_FOLDER)
+APP.config.from_object(__name__)
+
+
+@APP.route('/')
+def home():
+    '''Renders homepage.'''
+    return APP.send_static_file('index.html')
+
+
+@APP.route('/organisms/<term>')
+def get_organisms(term):
+    '''Gets organisms from search term.'''
+    return json.dumps(taxonomy_utils.search(term))
+
+
+@APP.route('/chemicals/<term>')
+def get_chemicals(term):
+    '''Gets organisms from search term.'''
+    return json.dumps([{'id': entity.get_id(), 'name': entity.get_name()}
+                       for entity in libchebipy.search(term)])
+
+
+@APP.route('/enzymes/<term>')
+def get_enzymes(term):
+    '''Gets organisms from search term.'''
+    uniprot_vals = sequence_utils.search_uniprot(term,
+                                                 ['entry name',
+                                                  'protein names',
+                                                  'organism',
+                                                  'sequence',
+                                                  'database(PDB)',
+                                                  'feature(BETA STRAND)',
+                                                  'feature(HELIX)',
+                                                  'feature(TURN)'],
+                                                 limit=10)
+
+    result = []
+
+    for key, value in uniprot_vals.iteritems():
+        pdb_key = 'Cross-reference (PDB)'
+        value[pdb_key] = [_get_pdb_data(pdb_id)
+                          for pdb_id in value.pop(pdb_key, '').split(';')
+                          if len(pdb_id) > 0]
+        value['id'] = key
+
+        value['Beta strand'] = _get_secondary_data(value.get('Beta strand',
+                                                             ''))
+        value['Helix'] = _get_secondary_data(value.get('Helix', ''))
+        value['Turn'] = _get_secondary_data(value.get('Turn', ''))
+
+        result.append(value)
+
+    return json.dumps(result)
+
+
+def _get_pdb_data(pdb_id):
+    '''Returns PDB sequence data.'''
+    url = 'http://www.rcsb.org/pdb/download/downloadFile.do' + \
+        '?fileFormat=FASTA&compression=NO&structureId=' + \
+        pdb_id
+
+    temp_file = tempfile.NamedTemporaryFile()
+    urllib.urlretrieve(url, temp_file.name)
+    fasta_seqs = SeqIO.parse(open(temp_file.name), 'fasta')
+
+    return {'id': pdb_id, 'chains': {fasta.id[len(pdb_id) + 1:len(pdb_id) + 2]:
+                                     str(fasta.seq)
+                                     for fasta in fasta_seqs}}
+
+
+def _get_secondary_data(strng):
+    '''Gets secondary structure data.'''
+    if len(strng) > 0:
+        fields = ['start', 'end', 'pdb']
+        return [dict(zip(fields, _parse_secondary_struct(s)))
+                for s in strng.split('.; ')]
+    return []
+
+
+def _parse_secondary_struct(strng):
+    '''Parses secondary structure string.'''
+    regex = r' (\d*) (\d*).*PDB:(\w*)'
+    terms = re.findall(regex, strng)[0]
+    return int(terms[0]), int(terms[1]), terms[2]
+
+
+@APP.errorhandler(Exception)
+def handle_exception(err):
+    '''Exception handling method.'''
+    response = jsonify({'message': err.__class__.__name__ + ': ' + str(err)})
+    response.status_code = 500
+    return response
---
+++ search/static/search/search.ctrl.js
@@ -0,0 +1,148 @@
+searchApp.controller("searchCtrl", function($scope) {
+	var self = this;
+	self.chemical = null;
+	self.enzyme = null;
+	self.organism = null;
+	self.residue = null;
+	self.pdbIndex = 0;
+	self.structureText = null;
+
+	self.range = function(min, max, step) {
+		step = step || 1;
+		var input = [];
+		for(i = min; i < max; i += step) {
+			input.push(i);
+		}
+		return input;
+	};
+	
+	
+	/* SEQUENCE */
+	self.secondaryStructure = [];
+	
+	$scope.$watch(angular.bind(this, function() {
+		return self.enzyme;
+	}), function(newVal) {
+		updateSecondaryStructure();
+	});
+	
+	updateSecondaryStructure = function() {
+		self.secondaryStructure = [];
+
+		if(self.enzyme) {
+			for(var i=0; i < self.enzyme.Sequence.length; i++) {
+				self.secondaryStructure.push("");
+			}
+
+			setSecondaryStructure(self.enzyme["Beta strand"], "beta-strand");
+			setSecondaryStructure(self.enzyme["Helix"], "helix");
+			setSecondaryStructure(self.enzyme["Turn"], "turn");
+		}
+	};
+
+	setSecondaryStructure = function(secStruct, className) {
+		for(var i = 0; i < secStruct.length; i++) {
+			obj = secStruct[i];
+
+			for(var j = obj["start"] - 1; j < obj["end"]; j++) {
+				self.secondaryStructure[j] = className;
+			}
+		}
+	};
+	
+	
+	/* STRUCTURE */
+	
+	var mol = null;
+	var geom = null;
+	var selectedAtoms = [];
+
+	var options = {
+			width: 'auto',
+			height: 'auto',
+			antialias: true,
+			quality: 'medium'
+	};
+	
+	var viewer = pv.Viewer(document.getElementById("structure-viewer"), options);
+
+	viewer.fitParent();
+
+	window.onresize = function(event) {
+		viewer.fitParent();
+	};
+	
+	$scope.$watch(angular.bind(this, function() {
+		return self.enzyme;
+	}), function(newVal) {
+		self.updateStructure(0);
+	});
+	
+	$scope.$watch(angular.bind(this, function() {
+		return self.residue;
+	}), function(newVal) {
+		highlightResidue();
+	});
+	
+	self.updateStructure = function(increment) {
+		clearStructure();
+
+		self.pdbIndex = self.pdbIndex + increment;
+
+		if(self.enzyme && self.enzyme['Cross-reference (PDB)'].length) {
+			pv.io.fetchPdb('http://files.rcsb.org/download/' + self.enzyme['Cross-reference (PDB)'][self.pdbIndex]['id'] + '.pdb', function(newMol) {
+				mol = newMol;
+				geom = viewer.cartoon('protein', mol, {color: color.bySS()});
+				selectedAtoms = [];
+				viewer.autoZoom();
+			});
+		}
+	};
+
+	clearStructure = function() {
+		mol = null;
+		geom = null;
+		selectedAtoms = [];
+		viewer.clear();
+		viewer.requestRedraw();
+		self.structureText = null;
+	}
+	
+	highlightResidue = function() {
+		if(mol) {
+			// Revert currently selected atoms:
+			for(var i = 0; i < selectedAtoms.length; i++) {
+				setColorForAtom(selectedAtoms[i].atom, selectedAtoms[i].color);
+			}
+
+			self.structureText = null;
+			selectedAtoms = [];
+
+			// Colour newly selected atoms in each chain:
+			if(self.residue) {
+				var chains = mol.chains();
+
+				for(var i = 0; i < chains.length; i++) {
+					var chainName = chains[i].name();
+					var atom = mol.atom(chainName + '.' + self.residue + '.CA');
+
+					if(atom !== null) {
+						var color = [0,0,0,0];
+						geom.getColorForAtom(atom, color);
+						selectedAtoms.push({atom: atom, color: color});
+						setColorForAtom(atom, 'red');
+						self.structureText = atom.qualifiedName().split(".")[1];
+					}
+				}
+			}
+
+			viewer.requestRedraw();
+		}
+	};
+
+	setColorForAtom = function(atom, color) {
+		var view = mol.createEmptyView();
+		view.addAtom(atom);
+		geom.colorBy(pv.color.uniform(color), view);
+	}
+});
